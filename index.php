e929b3dc7754baa29f9d476eb46ecf929aa63920
Usman Tahir <ch.usman.tahir@gmail.com>
2014-06-17 05:42:02 +0500

    Updated Index + sty;les

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <html>
     <title>Heroku App</title>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
     <body>
-        <div><?php echo "Heroku"; ?></div>
+        <div class="index">
+            <div class="title">SE-II Demo</div>
+            <div class="group">
+                <ul class="memberNames">
+                    <li>Usman Tahir</li>
+                    <li>Abrar Ashfaq</li>
+                    <li>Haris Nadeem</li>
+                    <li>Aqsa Anwar</li>
+                    <li>Aneesa Abbasi</li>
+                    <li>Mehreen Zahid</li>
+                </ul>
+            </div>
+        </div>
+        <?php echo "" ?>
     </body>
 </html>
