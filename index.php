8418c688f5d516fc8a8680d3556a8ebe28f8fe1b
root <root@usman>
2014-06-18 03:01:21 -0400

    test comit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
             <div class="title">SE-II Demo</div>
             <div class="group">
                 <ul class="memberNames">
-                    <li>Usman Tahir</li>
+                    <li>Usman Tahir :D</li>
                     <li>Abrar Ashfaq</li>
                     <li>Haris Nadeem</li>
                     <li>Aqsa Anwar</li>
